827d31e6ec679f696429a610332de3a6ec2f6d69
Fabrício Yamamoto <fabriciorby@hotmail.com>
2019-05-19 14:42:08 -0300

    midi working (disable adblock)

---
+++ src/components/Partitura.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="hello">
+    <h1>Vue Midi abcjs Demo</h1>
+    <textarea id="abc-source" v-model="tune"></textarea>
+    <div class="listener-output">
+      <div class="label">
+        Currently Playing:
+        <span class="abc-string">{{currentAbcFragment}}</span>
+      </div>
+
+      <div class="label">Parameters sent to listener callback:</div>
+      <div>Progress: {{progress.progress }}</div>
+      <div>Current Time: {{progress.currentTime }}</div>
+      <div>Total Duration: {{progress.duration }}</div>
+      <div>New Beat? {{progress.newBeat }}</div>
+    </div>
+    <div id="midi"></div>
+    <div id="paper"></div>
+  </div>
+</template>
+
+<script>
+import "font-awesome/css/font-awesome.min.css";
+import "abcjs/abcjs-midi.css";
+import abcjs from "abcjs/midi";
+
+export default {
+  mounted: function() {
+    new abcjs.Editor("abc-source", {
+      canvas_id: "paper",
+      generate_midi: true,
+      midi_id: "midi",
+      abcjsParams: {
+        midiListener: this.listener,
+        animate: {
+          listener: this.animate
+        }
+      }
+    });
+  },
+  name: "Partitura",
+  data() {
+    return {
+      progress: {},
+      currentAbcFragment: "(none)",
+      tune:
+        'X:1\nT: Cooley\'s\nM: 4/4\nL: 1/8\nR: reel\nK: Emin\nD2|:"Em"EB{c}BA B2 EB|~B2 AB dBAG|"D"FDAD BDAD|FDAD dAFD|\n"Em"EBBA B2 EB|B2 AB defg|"D"afe^c dBAF|1"Em"DEFD E2 D2:|2"Em"DEFD E2 gf||\n|:"Em"eB B2 efge|eB B2 gedB|"D"A2 FA DAFA|A2 FA defg|\n"Em"eB B2 eBgB|eB B2 defg|"D"afe^c dBAF|1"Em"DEFD E2 gf:|2"Em"DEFD E4|]\n'
+    };
+  },
+  methods: {
+    listener(midiControl, progress) {
+      // This provides a more linear view of the progress, for a progress bar or for an unrelated animation.
+      this.progress = progress;
+    },
+    colorRange(range, color) {
+      if (range && range.elements) {
+        range.elements.forEach(function(set) {
+          set.forEach(function(item) {
+            item.setAttribute("fill", color);
+          });
+        });
+      }
+    },
+    animate(lastRange, currentRange) {
+      // This provides the actual visual note being played. It can be used to create the "bouncing ball" effect.
+      this.colorRange(lastRange, "#000000"); // Set the old note back to black.
+      this.colorRange(currentRange, "#3D9AFC"); // Set the currently sounding note to blue.
+      if (currentRange)
+        this.currentAbcFragment = this.tune.substring(
+          currentRange.startChar,
+          currentRange.endChar
+        );
+      else this.currentAbcFragment = "(none)";
+    }
+  }
+};
+</script>
+
+<style>
+.hello {
+  text-align: center;
+}
+
+#abc-source {
+  width: 460px;
+  height: 160px;
+  padding: 6px;
+}
+
+.listener-output {
+  border: 1px solid #888888;
+  padding: 6px;
+  border-radius: 4px;
+  width: 460px;
+  margin-bottom: 20px;
+  margin: auto;
+}
+
+.abc-string {
+  border: 1px solid #e9ef96;
+  padding: 2px;
+  height: 24px;
+  width: 60px;
+  background: #fbf4b8;
+}
+
+pre {
+  border: 1px solid #888888;
+  padding: 6px;
+  border-radius: 4px;
+  width: 460px;
+}
+
+#midi {
+  width: 756px;
+  margin: auto;
+}
+
+.label {
+  font-weight: bold;
+}
+</style>
